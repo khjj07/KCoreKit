4555326299fb39c718fdc46cb9c3ac53d5a70f4b
DESKTOP-0PER6KS\khjj0 <khjj07@gmail.com>
2026-03-26 04:40:01 +0900

    feat : Request 시스템

---
+++ Scripts/GameSystem/RequestSystemBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KCoreKit
+{
+    public abstract class RequestSystemBase : GameSubSystemBase
+    {
+        private Queue<IRequest> _requestQueue = new Queue<IRequest>();
+        [SerializeField] private int maxRequestPerFrame = 10;
+
+        public void SendRequest(IRequest request)
+        {
+            _requestQueue.Enqueue(request);
+
+            StartCoroutine(request.Wait());
+        }
+
+        protected abstract void Respond(IRequest request);
+
+        public override IEnumerator OnUpdate()
+        {
+            yield return RespondRequestRoutine();
+        }
+
+        private IEnumerator RespondRequestRoutine()
+        {
+            for (int i = 0; i < maxRequestPerFrame; i++)
+            {
+                if (_requestQueue.Count == 0)
+                {
+                    break;
+                }
+
+                var request = _requestQueue.Dequeue();
+                Respond(request);
+                request.Proceed();
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Scripts/Interface/IRequest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace KCoreKit
+{
+    public interface IRequest
+    {
+        public IEnumerator Wait();
+        public void Proceed();
+    }
+
+    public class RequestBase : IRequest
+    {
+        private bool _isWaiting;
+        public Action<RequestBase> OnResponseCallback;
+        private IEnumerator _waitEnumerator;
+
+        public RequestBase()
+        {
+            _waitEnumerator = new WaitUntil(()=>!IsWaiting());
+        }
+        public IEnumerator Wait()
+        {
+            _isWaiting = true;
+            yield return _waitEnumerator;
+            OnResponse();
+        }
+
+        public void Proceed()
+        {
+            _isWaiting = false;
+        }
+     
+        public void OnResponse()
+        {
+           OnResponseCallback?.Invoke(this);
+        }
+        
+        public bool IsWaiting()
+        {
+            return _isWaiting;
+        }
+    }
+}
