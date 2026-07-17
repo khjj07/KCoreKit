b52b1aac0be1d1db6bcc39c779c5822506c303e4
DESKTOP-A2MHDHE\khjj0 <khjj07@gmail.com>
2026-07-18 06:45:00 +0900

    feat : 예외처리

--- Scripts/Common/BuildPipelineSetting.cs
+++ Scripts/Common/BuildPipelineSetting.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.Build.Reporting;
+#endif
 using UnityEngine;
 
 namespace KCoreKit
