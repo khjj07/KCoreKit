a11dfaec28e18db8d842cc936b8929c7129286ca
DESKTOP-A2MHDHE\khjj0 <khjj07@gmail.com>
2026-07-17 20:19:46 +0900

    feat : 폰트 변경 테스트

--- Scripts/Printer/Printer.cs
+++ Scripts/Printer/Printer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using DG.Tweening;
@@ -16,17 +17,26 @@ namespace KCoreKit
         private bool _isPlaying;
 
 
-        public void Setup(string text,TMP_FontAsset font = null)
+        public void Setup(string text, TMP_FontAsset font = null)
         {
             _textComponent = GetComponent<TMP_Text>();
-            if (font != null)
+            if (font)
             {
                 _textComponent.font = font;
             }
+
             _letters = GenerateLetter(text);
             _textComponent.text = GenerateText();
             _appearSequence = GenerateAppearSequence(_letters);
         }
+
+        public void OnEnable()
+        {
+            _textComponent = GetComponent<TMP_Text>();
+            _textComponent.fontSharedMaterials = PrinterManager.GetFontSharedMaterials();
+          
+        } 
+
 
         private string GenerateText()
         {
@@ -39,7 +49,7 @@ namespace KCoreKit
             return builder.ToString();
         }
 
-        public Tween Print(float delay = 0,TweenCallback callback = null)
+        public Tween Print(float delay = 0, TweenCallback callback = null)
         {
             if (_isPlaying)
             {
@@ -75,25 +85,23 @@ namespace KCoreKit
 
         public Sequence GenerateAppearSequence(Letter[] letters)
         {
-            var sequence = DOTween.Sequence().Pause().SetAutoKill(false);;
+            var sequence = DOTween.Sequence().Pause().SetAutoKill(false);
+            ;
             foreach (var letter in letters)
             {
-                sequence.Append(letter.AppearSequence().AppendCallback(() =>
-                {
-                    letter.RepeatSequence();
-                }));
+                sequence.Append(letter.AppearSequence().AppendCallback(() => { letter.RepeatSequence(); }));
             }
+
             return sequence;
         }
-        
+
         public Letter[] GenerateLetter(string text)
         {
-            var setting = PrinterManager.GetInstance();
             List<Letter> result = new List<Letter>();
             string pattern = @"<(?<tag>\w+)>(?<value>.*?)<\/\w+>|([^<>]+)";
             string pattern1 = @"<(?<tag>\w+)>(?<value>.*?)<\/\w+>";
 
-            MatchCollection matches = Regex.Matches(text, pattern);
+            MatchCollection matches = Regex.Matches(text, pattern, RegexOptions.Singleline);
             foreach (Match match in matches)
             {
                 var realMatch = Regex.Match(match.Value, pattern1);
@@ -104,7 +112,7 @@ namespace KCoreKit
 
                     if (sytleName != "")
                     {
-                        PrintStyle style = setting.FindDialogStyle(sytleName);
+                        PrintStyle style = PrinterManager.FindDialogStyle(sytleName);
                         foreach (var c in value)
                         {
                             result.Add(new Letter(c, style, _textComponent.color));
@@ -114,7 +122,7 @@ namespace KCoreKit
                     {
                         foreach (var c in value)
                         {
-                            result.Add(new Letter(c, setting.defaultStyle, _textComponent.color));
+                            result.Add(new Letter(c, PrinterManager.defaultStyle, _textComponent.color));
                         }
                     }
                 }
@@ -122,7 +130,7 @@ namespace KCoreKit
                 {
                     foreach (var c in match.Value)
                     {
-                        result.Add(new Letter(c, setting.defaultStyle, _textComponent.color));
+                        result.Add(new Letter(c, PrinterManager.defaultStyle, _textComponent.color));
                     }
                 }
             }
@@ -131,37 +139,51 @@ namespace KCoreKit
         }
 
         private void LateUpdate()
+
         {
             if (_letters != null && _textComponent.text.Length > 0)
             {
                 _textComponent.ForceMeshUpdate();
-                var mesh = _textComponent.mesh;
-                var textInfo = _textComponent.textInfo;
-                Vector3[] vertices = mesh.vertices;
-                Color[] colors = mesh.colors;
 
+                var mesh = _textComponent.mesh;
+
+                var textInfo = _textComponent.textInfo;
+
+                Vector3[] vertices = mesh.vertices;
+
+                Color[] colors = mesh.colors;
+                
+            
                 for (int i = 0; i < textInfo.characterCount; i++)
                 {
                     var characterInfo = textInfo.characterInfo[i];
+                    characterInfo.fontAsset = _letters[i].style.font;
+                    characterInfo.materialReferenceIndex = PrinterManager.GetStyleIndex(_letters[i].style);
+                    textInfo.characterInfo[i] = characterInfo;
+                    
                     if (!characterInfo.isVisible)
+
                     {
                         continue;
                     }
 
                     Vector3 center = Vector3.zero;
+
                     float halfHeight, halfWidth;
 
                     halfHeight = Vector3.Distance(vertices[characterInfo.vertexIndex],
-                        vertices[characterInfo.vertexIndex + 1]) / 2;
+                        vertices[characterInfo.vertexIndex + 1]) * _letters[i].scale.y / 2;
+
                     halfWidth = Vector3.Distance(vertices[characterInfo.vertexIndex + 1],
-                        vertices[characterInfo.vertexIndex + 2]) / 2;
+                        vertices[characterInfo.vertexIndex + 2]) * _letters[i].scale.x / 2;
+
 
                     for (int j = 0; j < 4; j++)
                     {
                         var origin = vertices[characterInfo.vertexIndex + j];
-
                         center += origin;
                     }
+
 
                     center /= 4;
 
@@ -169,27 +191,34 @@ namespace KCoreKit
                                                           Quaternion.Euler(_letters[i].rotation) *
                                                           new Vector3(-halfWidth * _letters[i].scale.x,
                                                               -halfHeight * _letters[i].scale.y, 0);
+
                     vertices[characterInfo.vertexIndex + 1] = center + _letters[i].position +
                                                               Quaternion.Euler(_letters[i].rotation) *
                                                               new Vector3(-halfWidth * _letters[i].scale.x,
                                                                   halfHeight * _letters[i].scale.y, 0);
+
                     vertices[characterInfo.vertexIndex + 2] = center + _letters[i].position +
                                                               Quaternion.Euler(_letters[i].rotation) *
                                                               new Vector3(halfWidth * _letters[i].scale.x,
                                                                   halfHeight * _letters[i].scale.y, 0);
+
                     vertices[characterInfo.vertexIndex + 3] = center + _letters[i].position +
                                                               Quaternion.Euler(_letters[i].rotation) *
                                                               new Vector3(halfWidth * _letters[i].scale.x,
                                                                   -halfHeight * _letters[i].scale.y, 0);
+
                     colors[characterInfo.vertexIndex] = _letters[i].color;
+
                     colors[characterInfo.vertexIndex + 1] = _letters[i].color;
+
                     colors[characterInfo.vertexIndex + 2] = _letters[i].color;
+
                     colors[characterInfo.vertexIndex + 3] = _letters[i].color;
                 }
-
                 mesh.colors = colors;
                 mesh.vertices = vertices;
                 _textComponent.canvasRenderer.SetMesh(mesh);
+                
             }
         }
 
