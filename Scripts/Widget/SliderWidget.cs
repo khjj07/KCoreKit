322e19618244a2735007c512c0ecef2bbb7f6643
khjj07@gmail.com <khjj07@gmail.com>
2026-08-14 19:12:34 +0900

    feat : Sliter 위젯

---
+++ Scripts/Widget/SliderWidget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace KCoreKit
+{
+    public class SliderWidget : WidgetBase
+    {
+        public Slider slider => GetComponent<Slider>();
+
+        public Action<float> onValueChanged;
+
+        public void Awake()
+        {
+            slider.onValueChanged.AddListener(x=>onValueChanged?.Invoke(x));
+        }
+
+        public void Setup(float min, float max, float current)
+        {
+            slider.minValue = min;
+            slider.maxValue = max;
+            slider.value = current;
+        }
+    }
+}
