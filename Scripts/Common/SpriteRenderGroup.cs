1843cff5ad658189f8edbcd24deeafcd89304a68
khjj07@gmail.com <khjj07@gmail.com>
2026-08-19 00:25:43 +0900

    chore : dotween asmdef 설정

---
+++ Scripts/Common/SpriteRenderGroup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+
+namespace KCoreKit
+{
+    public class SpriteRenderGroup : MonoBehaviour
+    {
+        
+        private List<SpriteRenderer> _spriteRenderers;
+        private bool _shown;
+
+        public void Awake()
+        {
+            _spriteRenderers = GetComponentsInChildren<SpriteRenderer>().ToList();
+        }
+
+        public void Fade(float alpha,float duration)
+        {
+            foreach (var spriteRenderer in _spriteRenderers)
+            {
+                spriteRenderer.DOFade(alpha, duration);
+            }
+        }
+    }
+}
