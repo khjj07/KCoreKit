f68a16954cfae352cf8eab97e01d98011d9f47b8
khjj07@gmail.com <khjj07@gmail.com>
2026-08-26 12:35:01 +0900

    feat : Canvas Extension

---
+++ Scripts/Extensions/CanvasExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace KCoreKit
+{
+    public static class CanvasExtension
+    {
+        public static void Open(this Canvas canvas)
+        {
+            canvas.gameObject.SetActive(true);
+        }
+        public static void Close(this Canvas canvas)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+}
